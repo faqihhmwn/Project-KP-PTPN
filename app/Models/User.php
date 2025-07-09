82d7a6f6cc10631a164d966d604ae982128f2ff7
Muhammad Faqih Himawan <muhammadfaqihhmwn@gmail.com>
2025-07-09 15:56:48 +0700

    login-dan-logout

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Unit;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'unit_id',
     ];
 
     /**
@@ -45,4 +47,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return redirect('/dashboard');
 });
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile-edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile-update', [ProfileController::class, 'update'])->name('profile.update');
 });
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+require __DIR__.'/auth.php';
